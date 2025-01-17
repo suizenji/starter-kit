f655b1bd088f0d524a7c5151f3bf186dd93a14b3
suizenji <suizenji@DESKTOP-GRCOHLF>
2025-01-17 17:06:31 +0900

    test

--- nextjs/catalogs/__tests__/hooks/useInfiniteScroll.spec.ts
+++ nextjs/catalogs/__tests__/hooks/useInfiniteScroll.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react';
-import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
+import { useInfiniteScroll, useGlobalScroll } from '@/hooks/useInfiniteScroll';
 
 describe('useInfiniteScroll', () => {
   let loadMore: jest.Mock;
@@ -28,7 +28,7 @@ describe('useInfiniteScroll', () => {
 
   it('event listenerの設定と解除', () => {
     const { result, rerender, unmount } = renderHook(() => {
-      const hookReturn = useMuiTableScroll({ loadMore, hasMore, isLoading });
+      const hookReturn = useInfiniteScroll({ loadMore, hasMore, isLoading });
       hookReturn.tableEl.current = mockDiv as any as HTMLDivElement;
       return hookReturn;
     });
@@ -45,7 +45,7 @@ describe('useInfiniteScroll', () => {
     };
 
     const { result, rerender, unmount } = renderHook(() => {
-      const hookReturn = useMuiTableScroll({ loadMore, hasMore, isLoading });
+      const hookReturn = useInfiniteScroll({ loadMore, hasMore, isLoading });
       hookReturn.tableEl.current = mockDiv as any as HTMLDivElement;
       return hookReturn;
     });
@@ -78,6 +78,77 @@ describe('useInfiniteScroll', () => {
 
     // isLoadingならスクロールが閾値を越えてもloadしない
     mockDiv.scrollTop = 900;
+    isLoading = true;
+    rerender();
+    act(() => {
+      scrollHandler();
+    });
+    expect(loadMore).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('useGlobalScroll', () => {
+  let loadMore: jest.Mock;
+  let hasMore: boolean;
+  let isLoading: boolean;
+
+  beforeEach(() => {
+    loadMore = jest.fn();
+    hasMore = true;
+    isLoading = false;
+  });
+
+  it('event listenerの設定と解除', () => {
+    window.addEventListener = jest.fn();
+    window.removeEventListener = jest.fn();
+
+    const { result, rerender, unmount } = renderHook(() => {
+      return useGlobalScroll({ loadMore, hasMore, isLoading });
+    });
+
+    expect(window.addEventListener).toHaveBeenCalled();
+    unmount();
+    expect(window.removeEventListener).toHaveBeenCalled();
+  });
+
+  it('スクロールが閾値を越えたらloadする', () => {
+    let scrollHandler: Function = () => {};
+    const addEventListener: any = (e: keyof WindowEventMap, fn: Function) => {
+      scrollHandler = fn;
+    };
+    window.addEventListener = addEventListener;
+
+    Object.defineProperty(document.body, 'clientHeight', { value: 1000 });
+    const setScrollY = (y: number) =>
+      Object.defineProperty(window, 'scrollY', { value: y });
+
+    const { result, rerender, unmount } = renderHook(() => {
+      return useGlobalScroll({ loadMore, hasMore, isLoading });
+    });
+
+    // 距離が閾値を超えないならloadしない
+    setScrollY(800);
+    act(() => {
+      scrollHandler();
+    });
+    expect(loadMore).not.toHaveBeenCalled();
+
+    // 距離が閾値を超えるとloadする
+    setScrollY(802);
+    act(() => {
+      scrollHandler();
+    });
+    expect(loadMore).toHaveBeenCalledTimes(1);
+
+    // 上方向のスクロールには反応しない
+    setScrollY(801);
+    act(() => {
+      scrollHandler();
+    });
+    expect(loadMore).toHaveBeenCalledTimes(1);
+
+    // isLoadingならスクロールが閾値を越えてもloadしない
+    setScrollY(802);
     isLoading = true;
     rerender();
     act(() => {
